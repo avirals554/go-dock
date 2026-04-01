3b7d7e04207fa9254bf6e1aec1c6207086914da6
Aviral <avirals554@Avirals-MacBook-Air.local>
2026-04-01 09:52:57 +0530

    fixing sytax errors

--- main.go
+++ main.go
@@ -9,24 +9,24 @@ import (
 
 func main() {
 	if os.Args[1] == "child" {
-	if err:=	syscall.Chroot("/mycontainer/rootfs"){
-		fmt.Println("there was a problem with the chroot syscall ")
-	return
+		if err := syscall.Chroot("/mycontainer/rootfs"); err != nil {
+			fmt.Println("there was a problem with the chroot syscall ")
+			return
 		}
-	if err:=	syscall.Chdir("/"){
-		fmt.Println(" there was a problem with the chdir syscall ")
-		return
-	}
+		if err := syscall.Chdir("/"); err != nil {
+			fmt.Println(" there was a problem with the chdir syscall ")
+			return
+		}
 		os.MkdirAll("/sys/fs/cgroup", 0755)
 
-	if err:=	syscall.Mount("proc", "/proc", "proc", 0, ""){
-		fmt.Println("there was a problem with the mount syscall ")
-		return
-	}
-	if err:=	syscall.Mount("cgroup2", "/sys/fs/cgroup", "cgroup2", 0, ""){
-		fmt.Println("there was an error with the 2nd mount syscall ")
-		return
-	}
+		if err := syscall.Mount("proc", "/proc", "proc", 0, ""); err != nil {
+			fmt.Println("there was a problem with the mount syscall ")
+			return
+		}
+		if err := syscall.Mount("cgroup2", "/sys/fs/cgroup", "cgroup2", 0, ""); err != nil {
+			fmt.Println("there was an error with the 2nd mount syscall ")
+			return
+		}
 		cmd := exec.Command("/bin/sh")
 
 		cmd.Stdin = os.Stdin
@@ -47,15 +47,16 @@ func main() {
 			Cloneflags: syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWUTS,
 		}
 
-	if 	err := cmd.Start(){
-		fmt.Println("there was an error with the command start thing ")
-	}
+		if err := cmd.Start(); err != nil {
+			fmt.Println("there was an error with the command start thing ")
+			return
+		}
 		id := cmd.Process.Pid
 		pidstr := fmt.Sprintf("%d", id)
 		os.WriteFile("/sys/fs/cgroup/mycontainer/cgroup.procs", []byte(pidstr), 0700)
 		os.WriteFile("/sys/fs/cgroup/mycontainer/memory.max", []byte("10485760"), 0700)
 
-		err = cmd.Wait()
+		err := cmd.Wait()
 		if err != nil {
 			panic(err)
 		}
