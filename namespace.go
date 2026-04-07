c22a69302ecdc71ea0cc425caf94e9cd51f022c2
Aviral <avirals554@Avirals-MacBook-Air.local>
2026-04-07 13:57:35 +0530

    trying to add the home directory for the users

--- namespace.go
+++ namespace.go
@@ -12,21 +12,32 @@ import (
 
 func child(argument string) {
 	rootfsPath := argument
+	//giving the path to the chroot
 	if err := syscall.Chroot(rootfsPath); err != nil {
 		fmt.Println("chroot failed:", err)
 		return
 	}
-	if err := syscall.Chdir("/"); err != nil {
+	if err := syscall.Mkdir("home", 0755); err != nil {
+		fmt.Println("root error", err)
+		return
+	}
+	if err := syscall.Mkdir("home/user", 0755); err != nil {
+		fmt.Println("root error", err)
+		return
+	}
+	if err := syscall.Chdir("/home/user"); err != nil {
 		fmt.Println("chdir failed:", err)
 		return
 	}
 	os.WriteFile("/etc/resolv.conf", []byte("nameserver 8.8.8.8\nnameserver 1.1.1.1\n"), 0644) // this is for addding the dns look up this checks the namesperver and finds the ip this has 2 nameservers one of google and the other of cloudfare
-
+	//creating a mount for the cgroup cause alpine doesnt have it ---
 	os.MkdirAll("/sys/fs/cgroup", 0755)
+	//mounting a new proc for the new process namespace that is to be creates
 	if err := syscall.Mount("proc", "/proc", "proc", 0, ""); err != nil {
 		fmt.Println("mount proc failed:", err)
 		return
 	}
+	// Mount cgroup2 so container processes can see their resource limit---
 	if err := syscall.Mount("cgroup2", "/sys/fs/cgroup", "cgroup2", 0, ""); err != nil {
 		fmt.Println("mount cgroup failed:", err)
 		return
@@ -47,3 +58,8 @@ func child(argument string) {
 		fmt.Println("shell error:", err)
 	}
 }
+
+//there are a few problems with this --
+// 1. what if we want to make more than one containers ?
+//  that would mean that every time we would create the same pair of veth right ? and that is a problem cause which is which ?
+//
